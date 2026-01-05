8d36054a08022a54e5dc6a3c8ef13031517ed636
Volodymyr Cholombytko <utkaduck156@duck.com>
2026-01-05 20:24:38 +0100

    Update HW31.py

--- IT_Career_Hub_Homeworks/Python_HW/HW31.py
+++ IT_Career_Hub_Homeworks/Python_HW/HW31.py
@@ -1,16 +1,22 @@
 #1
 import re
+"""
+print list of dates
+"""
 
 text = """The events N 123456 happened on
 15/03/2025, 01.12.2024 and 09-09-2023.
 Deadline: 28/02/2022."""
 
-print("Даты:", re.findall(r"(\d{4}-\d{2}-\d{2}|\d{2}/\d{2}/\d{4}|\d{2}.\d{2}.\d{4}|\d{2}-\d{2}-\d{4})",
+print("Dates:", re.findall(r"(\d{4}-\d{2}-\d{2}|\d{2}/\d{2}/\d{4}|\d{2}.\d{2}.\d{4}|\d{2}-\d{2}-\d{4})",
 text))
 
 #2
-
+"""
+print list of words 
+"""
 tag_input = "python, data-science / machine-learning; AI neural-networks"
 output_ = [x for x in re.split(r"[,\s./;]",tag_input) if x != ""]
 
-print("Вывод:", output_)
+
+print("Output:", output_)
