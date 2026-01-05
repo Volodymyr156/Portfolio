966e98d0b9930004fd8217a9609842143aa20358
Volodymyr Cholombytko <utkaduck156@duck.com>
2026-01-05 19:57:06 +0100

    Update HW21.py

--- IT_Career_Hub_Homeworks/Python_HW/HW21.py
+++ IT_Career_Hub_Homeworks/Python_HW/HW21.py
@@ -1,21 +1,18 @@
-""" 01 Повторения букв
+""" 01 Count characters of a string
 
-Реализуйте функцию, которая
-- принимает текст
-- и возвращает словарь с подсчётом количества каждой буквы, игнорируя регистр.
+Implement a function that
+- takes text
+- and returns a dictionary counting the number of each letter, ignoring case.
 
-Данные:
+The data:
 text = "Programming is fun!"
-Пример вывода:
+The output example:
 {'p': 1, 'r': 2, 'o': 1, 'g': 2, 'a': 1, 'm': 2, 'i': 2, 'n': 2, 's': 1, 'f': 1, 'u': 1}
 """
 
 from collections import Counter
 
 def char_count(text):
-        """
-        count only characters of a string
-        """
         t = text.lower()
         chars = Counter(t)
         for k in list(chars):
@@ -29,24 +26,21 @@ sample = {'p': 1, 'r': 2, 'o': 1, 'g': 2, 'a': 1, 'm': 2, 'i': 2, 'n': 2, 's': 1
 print(char_count(text))
 print(char_count(text) == sample)
 
-"""02 Группировка студентов по классам
+"""02 Group students by class
 
-Создайте структуру для группировки студентов по классам.
-Добавьте студентов в соответствующие группы.
+Create a function to group students by class.
+Add students to the appropriate groups.
 
-Данные:
+The data:
 students = [("class1", "Alice"), ("class2", "Bob"), ("class1", "Charlie"), ("class3", "Daisy")]
 
-Пример вывода:
+The output:
 {'class1': ['Alice', 'Charlie'], 'class2': ['Bob'], 'class3': ['Daisy']}
 """
 
 from collections import defaultdict
 
 def get_students_groups(students):
-    """
-    group students by classes
-    """
     newdict = defaultdict(list)
     for student in students:
         newdict[student[0]].append(student[1])
@@ -59,4 +53,5 @@ sample = {'class1': ['Alice', 'Charlie'], 'class2': ['Bob'], 'class3': ['Daisy']
 
 
 print(get_students_groups(students))
+
 print(get_students_groups(students) == sample)
